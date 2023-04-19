2905023fea9bcfc1932ad400f1e6ebd697fe1b7c
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-04-19 15:55:52 +0500

    Create function for adding new like and fetch number of like from APi

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -1,32 +1,45 @@
 import itemsCounter from './counter.js';
+import { likes, likesDisplay } from './likesCount.js';
 
 const counter = document.getElementById('add-new-link');
 const container = document.getElementById('smoothie-container');
 const fetchData = async () => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=60');
-  const data = await response.json();
-  const myArray = data.results;
-  // console.log(myArray);
-  myArray.map(async (i) => {
-    const response = await fetch(i.url);
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=42');
+    // console.log(response);
     const data = await response.json();
     // console.log(data);
-    const myCard = document.createElement('div');
-    myCard.classList = 'myCard';
-    myCard.id = `${data.id}`;
-    myCard.innerHTML = `<div class="cardContent"> 
+    const myArray = data.results;
+    // console.log(myArray);
+    const LoadMyArray = myArray.map(async (element) => {
+      const response = await fetch(element.url);
+      const data = await response.json();
+      // console.log(data);
+      const myCard = document.createElement('div');
+      myCard.classList = 'myCard';
+      myCard.id = `${data.id}`;
+      myCard.innerHTML = `<div class="cardContent"> 
     <img src="${data.sprites.back_default}" alt="${data.name}" class="cardImg">
-    <div class="cardHeader">
+    <div class="cardTitle">
     <h2>${data.name}</h2>
     </div>
     <div class="interation">
     <button class='likeBtn' id="likeBtn"><i class="fa-regular fa-thumbs-up"></i></button>
-    <span class='counter'>0</span>
+    <span class='likeCounter'>0</span>
     <button class="commentBtn" id="commentBtn">Comments</button>
     </div>
     `;
-    container.appendChild(myCard);
-  });
-  itemsCounter(myArray.length, counter);
+      container.appendChild(myCard);
+    });
+    await Promise.all(LoadMyArray);
+    likes();
+    likesDisplay();
+    itemsCounter(myArray.length, counter);
+  } catch (error) {
+    const errorMessage = 'Error, try again later.';
+    const errorElement = document.createElement('div');
+    errorElement.innerText = errorMessage;
+    document.body.appendChild(errorElement);
+  }
 };
 export default fetchData;
---
+++ src/modules/likesCount.js
@@ -0,0 +1,46 @@
+const involvementApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/sExEVU49eBfzgZOnwbX2/likes';
+
+const likes = async () => {
+  const likeCount = document.querySelectorAll('.likeCounter');
+  const cards = document.querySelectorAll('.myCard');
+  await fetch(involvementApi)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      const cardsArray = Array.from(cards);
+      // console.log(cardsArray);
+      cardsArray.forEach((myCard, index) => {
+        data.map((item) => {
+          if (item.item_id === myCard.id) {
+            likeCount[index].innerHTML = item.likes;
+          }
+          return likeCount[index].innerHTML;
+        });
+      });
+    });
+};
+
+const likesDisplay = async () => {
+  const likeBtns = document.querySelectorAll('.likeBtn');
+  const thumbsUp = document.querySelectorAll('.fa-thumbs-up');
+  const likesCounter = document.querySelectorAll('.likeCounter');
+  const cards = document.querySelectorAll('.myCard');
+  likeBtns.forEach((btn, index) => {
+    btn.addEventListener('click', async () => {
+      thumbsUp[index].classList.remove('fa-regular');
+      thumbsUp[index].classList.add('fa-solid');
+      likesCounter[index].innerHTML = Number(likesCounter[index].innerHTML) + 1;
+      await fetch(involvementApi, {
+        method: 'POST',
+        body: JSON.stringify({
+          item_id: cards[index].id,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+    });
+  });
+};
+
+export { likes, likesDisplay };
