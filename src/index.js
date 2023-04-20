b3b787da4e88fd4422ebbc0fb6583c8db4aa317d
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-04-21 01:50:47 +0500

    Initial commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import fetchData from './modules/displayData.js';
-
-fetchData();
-const logo = require('../sources/mylogo.png');
-
-const img = document.getElementById('img');
-img.src = logo;
--- src/modules/counter.js
+++
@@ -1,5 +0,0 @@
-const itemsCounter = (count, counter) => {
-  counter.innerHTML = `Items count = ${count}`;
-  return count;
-};
-export default itemsCounter;
