7f882cf8fdb056b34bdf9baaaded814067117793
Joy Phoebe <joyapisi@gmail.com>
2023-04-20 03:57:51 +0300

    display modal for comments

---
+++ src/modules/comments.js
@@ -0,0 +1,30 @@
+const commentsPopup = () => {
+  // Open reservation modal
+  const openModalBtn = document.querySelectorAll('[data-modal-target]');
+  openModalBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const dialogContainer = document.querySelector(btn.dataset.modalTarget);
+      document.body.classList.add('no-scroll');
+      dialogContainer.classList.add('dialog-container');
+      if (!dialogContainer.open) {
+        dialogContainer.showModal();
+      }
+    });
+  });
+  // Close reservation modal
+  document.addEventListener('DOMContentLoaded', () => {
+    const closeButtons = document.querySelectorAll('[data-close-button]');
+    closeButtons.forEach((closeButton) => {
+      closeButton.addEventListener('click', () => {
+        const dialogContainer = document.querySelector('.dialog-container');
+        if (dialogContainer) {
+          const dialog = dialogContainer.querySelector('dialog');
+          dialog.close();
+          dialogContainer.classList.remove('dialog-container');
+          document.body.classList.remove('no-scroll');
+        }
+      });
+    });
+  });
+};
+export default commentsPopup;
--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -1,5 +1,6 @@
 import itemsCounter from './counter.js';
 import { likes, likesDisplay } from './likesCount.js';
+import commentsPopup from './comments.js';
 
 const counter = document.getElementById('add-new-link');
 const container = document.getElementById('smoothie-container');
@@ -19,7 +20,7 @@ const fetchData = async () => {
       myCard.classList = 'myCard';
       myCard.id = `${data.id}`;
       // console.log(data.id);
-      myCard.innerHTML = `<div class="cardContent"> 
+      myCard.innerHTML = `<div class="cardContent">
     <img src="${data.sprites.back_default}" alt="${data.name}" class="cardImg">
     <div class="cardTitle">
     <h2>${data.name}</h2>
@@ -27,14 +28,47 @@ const fetchData = async () => {
     <div class="interation">
     <button class='likeBtn' id="likeBtn"><i class="fa-regular fa-thumbs-up"></i></button>
     <span class='likeCounter'>0</span>
-    <button class="commentBtn" id="commentBtn">Comments</button>
+    <button data-modal-target="#popup${data.id}" class="comments-btn" id='${data.id}'>Comments</button>
     </div>
+    
+    <dialog id='popup${data.id}' class='comments-popup'>
+    <button type='button' data-close-button id='closeBtn' class='closeBtn' title='closeBtn'>
+     <a> &times;</a>
+    </button>
+    <img src="${data.sprites.front_default}" alt="${data.name}" id="popup-img">
+    <div class="comments-topic">
+      <h2>${data.name.toUpperCase()}</h2>
+    </div>
+    <div class="popup-content">
+      <div class="contents">
+        <div class="first-contents" >
+          <p>Weight: ${data.weight}</p>
+          <p>Height: ${data.height}</p>
+        </div>
+        <div class="second-contents">
+          <p>Base experience: ${data.base_experience}</p>
+          <p>Order: ${data.order}</p>
+        </div>
+      </div>
+      <h2 class="comments-number" id="comments-number">Comments (0)</h2>
+      <div class="comments-display" id="comments-display"></div>
+    </div>
+    <div id = "add-comments">
+    <h3>Add a comment</h3>      
+      <form id = 'comments-form'>
+        <input id="name" type="text" placeholder="Your Name" required>
+        <textarea id="comment" name="your-insights" placeholder="your-insights"></textarea>
+        <button id="submit" type="submit">Comment</button>
+      </form>
+  </div> 
+  </dialog>
     `;
       container.appendChild(myCard);
     });
     await Promise.all(LoadMyArray);
     likes();
     likesDisplay();
+    commentsPopup();
     itemsCounter(myArray.length, counter);
   } catch (error) {
     const errorMessage = 'Error, try again later.';
