18b27fce7e13bfaa2fa9a22f000bc0459c916f14
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-04-21 03:27:23 +0500

    Try to fix lighthiuse errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
-/* eslint-disable */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'none',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
-  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -22,18 +25,15 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Development',
-        template: './src/index.html',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   optimization: {
     runtimeChunk: 'single',
